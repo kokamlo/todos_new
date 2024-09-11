d1521ecada4153c85d855dd563e163f221b0fa5a
kokamlo <behzad0hosseinzadeh@gmail.com>
2024-09-11 15:07:37 +0330

    vene

---
+++ gui.py
@@ -0,0 +1,10 @@
+import func
+import FreeSimpleGUI as fsg
+
+lable = fsg.Text("type in a todo")
+input_box = fsg.InputText('enter todo')
+add_buttom = fsg.Button('add')
+
+window =fsg.Window('My To-Do app' , layout=[[lable] , [input_box , add_buttom]])
+window.read()
+window.close()
