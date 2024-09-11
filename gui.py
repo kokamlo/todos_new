e431276ee949b5cb67521bd7f3e61fa6fb1eac90
kokamlo <behzad0hosseinzadeh@gmail.com>
2024-09-11 15:51:11 +0330

    gui 1

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ lable = fsg.Text("type in a todo")
 input_box = fsg.InputText('enter todo')
 add_buttom = fsg.Button('add')
 
-window =fsg.Window('My To-Do app' , layout=[[lable] , [input_box , add_buttom]])
+window =fsg.Window('My To-Do app' , layout=[[lable,input_box , add_buttom]])
 window.read()
 window.close()
